b4ca8146102f1e83e313484b08eea718977b94a6
ashi-5 <ashithkumar.nexmind@gmail.com>
2024-10-17 17:38:42 +0530

    cart quantity made interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -82,11 +82,14 @@ button.addEventListener('click',()=>{
       });
   }
 
+ let cartQuantity = 0;
+
+  cart.forEach((item)=>{
+cartQuantity += item.quantity;
+  })
+
+  document.querySelector('.js-cart-quantity').innerHTML=cartQuantity
 
 
-
-
-  
-  console.log(cart);
 })
 });
